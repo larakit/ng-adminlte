6a7df4fe63653748468fd6a5aca3af86f98338dc
Alexey Berdnikov <aberdnikov@gmail.com>
2018-01-05 01:13:06 +0500

    добавил возможность отключения дефолтного шаблона админки

--- boot/routes/page-admin-account.php
+++ boot/routes/page-admin-account.php
@@ -10,28 +10,30 @@
 //$url = \Larakit\LkNgRoute::adminUrl('colors');
 //\Larakit\LkNgRoute::proxy($url);
 
-$adminlte_callback = function (\Larakit\Page\LkPage $page) {
-    $page = \Larakit\Page\LkPage::instance()
-        ->setBodyContent('<div class="wrapper">
+if(!env('ADMINLTE_NO_ROUTE')) {
+    
+    $adminlte_callback = function (\Larakit\Page\LkPage $page) {
+        $page = \Larakit\Page\LkPage::instance()
+            ->setBodyContent('<div class="wrapper">
     <ng-view></ng-view>
 </div>');
-    $page->body()
-        ->setAttribute('style', 'height: auto; min-height: 100%;')
-        ->addClass('skin-black')->setAttribute('ng-class', '{
+        $page->body()
+            ->setAttribute('style', 'height: auto; min-height: 100%;')
+            ->addClass('skin-black')->setAttribute('ng-class', '{
         \'sidebar-collapse\':leftValue(),
         \'control-sidebar-open\':rightValue(),
         \'sidebar-open\':!leftValue(),
         \'control-sidebar-open\':rightValue(),
 }');
-    
-};
-\Larakit\LkNgRoute::proxy(\Larakit\LkNgRoute::accountUrl() . '{path?}', true, $adminlte_callback)
-    ->name('account')
-    ->middleware('ng-larakit')
-    ->where('path', '.*');
-\Larakit\LkNgRoute::proxy(\Larakit\LkNgRoute::adminUrl() . '{path?}', true, $adminlte_callback)
-    ->name('admin')
-    ->middleware('admin')
-    ->middleware('ng-larakit')
-    ->where('path', '.*');
-
+        
+    };
+    \Larakit\LkNgRoute::proxy(\Larakit\LkNgRoute::accountUrl() . '{path?}', true, $adminlte_callback)
+        ->name('account')
+        ->middleware('ng-larakit')
+        ->where('path', '.*');
+    \Larakit\LkNgRoute::proxy(\Larakit\LkNgRoute::adminUrl() . '{path?}', true, $adminlte_callback)
+        ->name('admin')
+        ->middleware('admin')
+        ->middleware('ng-larakit')
+        ->where('path', '.*');
+}
