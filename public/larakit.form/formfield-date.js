3216682e480c132f3a9b7cd5124c6800970accd6
Бердников Алексей <berdnikov_ay@bunkermedia.ru>
2018-06-27 18:06:10 +0500

    fixed

--- public/larakit.form/formfield-date.js
+++ public/larakit.form/formfield-date.js
@@ -1,7 +1,7 @@
 angular
     .module('larakit.form')
     .component('formfieldDate', {
-        templateUrl: '/packages/larakit/ng-adminlte/larakit.form/formfield-date.html?'+Math.random(),
+        templateUrl: '/packages/larakit/ng-adminlte/larakit.form/formfield-date.html?' + Math.random(),
         transclude: true,
         bindings: {
             error: '=',
@@ -17,16 +17,19 @@ angular
             self.opened = false;
             self.dateOptions = {
                 formatYear: 'yy',
-                maxDate: (undefined!==self.max)?new Date(self.max):null,
-                minDate: (undefined!==self.min)?new Date(self.min):null,
+                maxDate: (undefined !== self.max) ? new Date(self.max) : null,
+                minDate: (undefined !== self.min) ? new Date(self.min) : null,
                 startingDay: 1
             };
-            self.$postLink = function(){
-                self.model = new Date(self.model);
+            self.$postLink = function () {
+                if (self.model) {
+                    self.model = new Date(self.model);
+                }
+
             };
 
             self.format = 'dd.MM.yyyy';
-            self.clear = function(){
+            self.clear = function () {
 
             }
             self.onChange = function () {
@@ -35,4 +38,32 @@ angular
                 }
             };
         }
+    })
+    .directive('datetimepickerNeutralTimezone', function () {
+        return {
+            restrict: 'A',
+            priority: 1,
+            require: 'ngModel',
+            link: function (scope, element, attrs, ctrl) {
+                ctrl.$formatters.push(function (value) {
+                    if (value) {
+                        var date = new Date(Date.parse(value));
+                        date = new Date(date.getTime() + (60000 * date.getTimezoneOffset()));
+                        return date;
+                    } else {
+                        return null;
+                    }
+                });
+
+                ctrl.$parsers.push(function (value) {
+                    if (value) {
+                        var date = new Date(value.getTime() - (60000 * value.getTimezoneOffset()));
+                        return date;
+                    } else {
+                        return null;
+                    }
+                });
+            }
+        };
     });
+;
